a3b0937463d25ab552917fa1650a0c2e5616d3e5
wuhy <962491617@qq.com>
2018-07-13 17:08:12 +0800

    更新

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'           => 'mysql',
     // 服务器地址
-    'hostname'       => '127.0.0.1',//120.27.118.193
+    'hostname'       => '120.27.118.193',//120.27.118.193
     // 数据库名
     'database'       => 'w_shop',
     // 用户名
-    'username'       => 'root',//zhuxun
+    'username'       => 'zhuxun',//zhuxun
     // 密码
-    'password'       => 'root',//admin
+    'password'       => 'admin',//admin
     // 端口
     'hostport'       => '3306',
     // 连接dsn
